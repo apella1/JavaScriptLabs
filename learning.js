c81f8d69e9945d78321dec69fe616d38cc351cff
apella1 <johncollons5@gmail.com>
2022-08-28 20:10:21 +0300

    reviewed javascript classes

--- learning.js
+++ learning.js
@@ -33,7 +33,9 @@ const studentDetails = {
 
 console.log(studentDetails.studentProfile());
 
-const studentInformation = {...studentDetails};
+const studentInformation = {...studentDetails}; // using the spread operator to assign a variable an already declared variable. 
+
+console.log(studentInformation); 
 
 studentInformation.firstName = "Peters";
 
@@ -73,6 +75,29 @@ function Games(gameName, yearPublished, brandAmbassador) {
     }
 };
 
+
+function Cars (brand, manufacturer, price) { 
+    this.brand = brand;
+    this.manufacturer = manufacturer;
+    this.price = price;
+}
+
+const firstCar = new Cars ('Mercedes Benz', 'Mercedes', 34455);
+
+console.log(firstCar); 
+
+// using the spread operator to reassign the value of firstCar
+
+secondCar = {...firstCar};
+console.log(secondCar);
+
+secondCar.brand = 'Audi';
+console.log(secondCar);
+
+secondCar['Years Praised'] = [2000, 2034, 1975];
+
+console.log(secondCar);
+
 const game1 = new Games ("Football", 1467, "John Apella");
 console.log(game1.summary());  // making sure to indicate the additional double brackets for the function 
 
@@ -107,6 +132,15 @@ const preferredCandidate = Object.assign({}, myCandidate);
 
 console.log(preferredCandidate);
 
+
+const tools = {
+    name: 'hammer', 
+    weight: 79,
+}
+
+const machinery = Object.assign({}, tools);
+
+console.log(machinery);
 // Creating object copies using the spread operator, represented by three dots 
 
 let myBio1 = {...myBio};
@@ -119,12 +153,13 @@ console.log(myBio1); // with the spread operator, a change in the value of myBio
 let object2 = Object.assign({}, myBio); // the essence of the empty curly brackets 
 console.log(object2);
 
-console.log(object2 === myBio);
+console.log(object2 === myBio); // ? why are the two objects not strictly equal when one object has been completely mapped into the other
 
 
 let person1 = { 
     name:"Karl",
     age: 78,
+    sports: ['football', 'hockey', 'basketball'],
     gadgets: { 
         brands: ["Samsung", "Motorola"]
     }
@@ -142,6 +177,7 @@ console.log(person1);
 
 person2.gadgets.brands[1] = "Sony";
 
+person2.sports[2] = 'badminton';
 console.log(person2);
 
 console.log(person1); // changing the value of deeply nested objects from a replicated class affects the value of the initial class
@@ -160,6 +196,8 @@ console.log(person1); // changing the value of deeply nested objects from a repl
 
 // program to add the first ten numbers from 1
 
+// what is the += syntax?
+
 let total = 0, count = 1;
 while (count <=10) { 
     total += count;
@@ -173,6 +211,11 @@ for (let j = 1; j <=10; j++) {
     console.log(j);
 }
 
+let myNumbers =  [1, 2, 4, 5, 6, 8, 6, 8];
+
+const numbers = myNumbers.forEach( number => number.myNumbers); // can we loop over in such a way when the array isn't made up of objects with different values defined 
+
+console.log(numbers);
 
 let summation = 0, countSpace = 1;
 while (countSpace <=12) {
@@ -195,6 +238,19 @@ function factorial(m) {
 console.log(factorial(4));
 
 
+// an expression such as the one below cannot be hoisted as opposed to a function declaration which can
+const numFactorial = (g) => { 
+    if (g===0) { 
+        return 1;
+    } else { 
+        return numFactorial(g-1) * g;
+    }
+
+}
+
+console.log(numFactorial(5));
+
+
 // escaping characters 
 console.log("This is a line of text \nThis, however is another line of text written on a new line."); // the escaping character is a forward slash, not to be confused with the backward slash which is the division operator 
 console.log("David said, \"How are you doing today?\".");
@@ -208,6 +264,9 @@ console.log(typeof factorial); // typeof is referred to as a unary operator. The
 
 console.log(NaN === NaN); // NaN is the only value which isn't equal to itself. The log message returns false 
 console.log(!true); // '!' is the not operator 
+console.log(true === true);
+console.log(true == 1);
+console.log(false == 0);
 
 
 // demonstrating null and undefined empty values 
@@ -225,6 +284,9 @@ console.log(false === 0); // the normal equality sign yields true
 console.log(null || "Agnes");
 console.log("Peter" || "John");
 console.log(null || undefined);
+console.log(null && undefined);
+console.log('Peter' && 'John');
+
 
 // && works the opposite way to the || operator.
 // If the value to its left is false it returns that value, otherwise it returns the value to the right. 
@@ -275,7 +337,7 @@ const tags = document.getElementsByTagName("nav");
 console.log(tags);
 
 
-document.getElementsByName("");  // what are element names?
+document.getElementsByName("");  // the value of the name attribute for the tags with the name attribute such as the input tag
 
 const container = document.querySelector("div"); // selects the first item that matches the destination given. In this case it selects the first div element within the document model.
 console.log(container);
@@ -290,8 +352,7 @@ console.log(myTitle);
 
 // myTitle.style.color = "blue"; // inline styling. in JavaScript the css properties are written in camel case as opposed to the kebab case in css styling. For example text-transform becomes textTransform
 // myTitle.style.textTransform = "capitalize";
-// the commented out code is not running to change the inline styling of the html elements selected and assigned to the named variables. 
-console.log(myTitle); 
+// the commented out code is not running to change the inline styling of the html elements selected and assigned to the named variables.
 
 // inline styling of the elements doesn't happen for multiple elements. for that to be done the list elements have to be looped and the styles applied to every individual function
 
@@ -335,6 +396,6 @@ console.log(ul);
 
 // control flow - conditional execution 
 
-let numSqrt = Number(prompt("Pick a number")); // the prompt appears as an alert message prompting to enter a number
+let numSqrt = Number(prompt('Pick a number')); // the prompt appears as an alert message prompting to enter a number
 console.log(`Half the number you've picked is ${numSqrt * 0.5}`); // how to write exponential in js.
 
